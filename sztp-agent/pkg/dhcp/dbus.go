924f42d3059b0f4558b3c0377ad3e7835ece5122
Bhoopesh <bhoopesh459@gmail.com>
2024-09-06 18:22:03 +0530

    wip: nm dbus

--- sztp-agent/pkg/dhcp/dbus.go
+++ sztp-agent/pkg/dhcp/dbus.go
@@ -7,3 +7,57 @@ Copyright (C) 2022 Red Hat.
 
 // Package dhcp implements the DHCP client
 package dhcp
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/godbus/dbus"
+)
+
+func GetBootstrapURLsViaNetworkManager() ([]string, error) {
+	conn, err := dbus.SystemBus()
+	if err != nil {
+		panic(fmt.Errorf("failed to connect to system bus: %v", err))
+	}
+
+	// Get NetworkManager object
+	nm := conn.Object("org.freedesktop.NetworkManager", "/org/freedesktop/NetworkManager")
+
+	// Get PrimaryConnection property from NetworkManager object
+	var primaryConnPath dbus.ObjectPath
+	err = nm.Call("org.freedesktop.DBus.Properties.Get", 0, "org.freedesktop.NetworkManager", "PrimaryConnection").Store(&primaryConnPath)
+	if err != nil {
+		panic(fmt.Errorf("failed to get PrimaryConnection property: %v", err))
+	}
+
+	
+	// Get Active Connection object
+	connActive := conn.Object("org.freedesktop.NetworkManager", primaryConnPath)
+	
+	// Get Dhcp4Config property from Active Connection object
+	var dhcpPath dbus.ObjectPath
+	err = connActive.Call("org.freedesktop.DBus.Properties.Get", 0, "org.freedesktop.NetworkManager.Connection.Active", "Dhcp4Config").Store(&dhcpPath)
+	if err != nil {
+		panic(fmt.Errorf("failed to get Dhcp4Config property: %v", err))
+	}
+	
+	// Get Options property from DHCP4Config object
+	dhcp := conn.Object("org.freedesktop.NetworkManager", dhcpPath)
+	var options map[string]dbus.Variant
+	err = dhcp.Call("org.freedesktop.DBus.Properties.Get", 0, "org.freedesktop.NetworkManager.DHCP4Config", "Options").Store(&options)
+	if err != nil {
+		panic(fmt.Errorf("failed to get Options property: %v", err))
+	}
+	
+	// Print sztp_redirect_urls option
+	// sztpRedirectURLs := options["sztp_redirect_urls"].Value().(string)
+	if variant, ok := options["sztp_redirect_urls"]; ok {
+		sztpRedirectURLs := variant.Value().(string)
+		log.Println("lmao")
+		fmt.Println(sztpRedirectURLs)
+		return []string{sztpRedirectURLs}, nil
+	}
+
+	return nil, fmt.Errorf("sztp_redirect_url was not found")
+}
--- sztp-agent/pkg/secureagent/daemon.go
+++ sztp-agent/pkg/secureagent/daemon.go
@@ -51,31 +51,44 @@ func (a *Agent) performBootstrapSequence() error {
 	if err != nil {
 		return err
 	}
-	err = a.doRequestBootstrapServerOnboardingInfo()
-	if err != nil {
-		return err
+
+	urls := a.GetBootstrapURL()
+	log.Println("[INFO] Performing the Bootstrap Sequence on each URL")
+	for _, url := range urls {
+		urlCopy := url
+		err = a.doRequestBootstrapServerOnboardingInfo(&urlCopy)
+		if err != nil {
+			log.Println("[ERROR] ", err.Error())
+			continue
+		}
+		err = a.doHandleBootstrapRedirect(&urlCopy)
+		if err != nil {
+			log.Println("[ERROR] ", err.Error())
+			continue
+		}
+		err = a.downloadAndValidateImage(&urlCopy)
+		if err != nil {
+			log.Println("[ERROR] ", err.Error())
+			continue
+		}
+		err = a.copyConfigurationFile(&urlCopy)
+		if err != nil {
+			log.Println("[ERROR] ", err.Error())
+			continue
+		}
+		err = a.launchScriptsConfiguration(PRE, &urlCopy)
+		if err != nil {
+			log.Println("[ERROR] ", err.Error())
+			continue
+		}
+		err = a.launchScriptsConfiguration(POST, &urlCopy)
+		if err != nil {
+			log.Println("[ERROR] ", err.Error())
+			continue
+		}
+		_ = a.doReportProgress(ProgressTypeBootstrapComplete, "Bootstrap Complete", &urlCopy)
 	}
-	err = a.doHandleBootstrapRedirect()
-	if err != nil {
-		return err
-	}
-	err = a.downloadAndValidateImage()
-	if err != nil {
-		return err
-	}
-	err = a.copyConfigurationFile()
-	if err != nil {
-		return err
-	}
-	err = a.launchScriptsConfiguration(PRE)
-	if err != nil {
-		return err
-	}
-	err = a.launchScriptsConfiguration(POST)
-	if err != nil {
-		return err
-	}
-	_ = a.doReportProgress(ProgressTypeBootstrapComplete, "Bootstrap Complete")
+
 	return nil
 }
 
@@ -83,8 +96,9 @@ func (a *Agent) discoverBootstrapURLs() error {
 	log.Println("[INFO] Discovering the Bootstrap URL")
 	if a.InputBootstrapURL != "" {
 		log.Println("[INFO] User gave us the Bootstrap URL: " + a.InputBootstrapURL)
-		a.SetBootstrapURL(a.InputBootstrapURL)
-		log.Println("[INFO] Bootstrap URL retrieved successfully: " + a.GetBootstrapURL())
+		a.SetBootstrapURL([]string{a.InputBootstrapURL})
+		log.Println("[INFO] Bootstrap URL retrieved successfully: ")
+		log.Println(a.GetBootstrapURL())
 		return nil
 	}
 	if a.DhcpLeaseFile != "" {
@@ -93,17 +107,24 @@ func (a *Agent) discoverBootstrapURLs() error {
 		if err != nil {
 			return err
 		}
-		a.SetBootstrapURL(urls[0])
-		log.Println("[INFO] Bootstrap URL retrieved successfully: " + a.GetBootstrapURL())
+		a.SetBootstrapURL(urls)
+		log.Println("[INFO] Bootstrap URL retrieved successfully: ")
+		log.Println(a.GetBootstrapURL())
 		return nil
 	}
 	log.Println("[INFO] User gave us nothing, discover the Bootstrap URL from Network Manager via dbus")
 	// TODO: fetch the Bootstrap URL from Network Manager via dbus in the future
-	log.Println("[INFO] Bootstrap URL retrieved successfully: " + a.GetBootstrapURL())
+	urls, err := dhcp.GetBootstrapURLsViaNetworkManager()
+	if err != nil {
+		return err
+	}
+	a.SetBootstrapURL(urls)
+	log.Println("[INFO] Bootstrap URL retrieved successfully: ")
+	log.Println(a.GetBootstrapURL())
 	return nil
 }
 
-func (a *Agent) doHandleBootstrapRedirect() error {
+func (a *Agent) doHandleBootstrapRedirect(bootstrapURL *string) error {
 	if reflect.ValueOf(a.BootstrapServerRedirectInfo).IsZero() {
 		return nil
 	}
@@ -122,26 +143,26 @@ func (a *Agent) doHandleBootstrapRedirect() error {
 		return errors.New("invalid port")
 	}
 	// Change URL to point to new redirect IP and PORT
-	u, err := url.Parse(a.GetBootstrapURL())
+	u, err := url.Parse(*bootstrapURL)
 	if err != nil {
 		return err
 	}
 	u.Host = fmt.Sprintf("%s:%d", addr, port)
-	a.SetBootstrapURL(u.String())
+	*bootstrapURL = u.String()
 
-	// Request onboard ino again (with new URL now)
-	return a.doRequestBootstrapServerOnboardingInfo()
+	// Request onboard info again (with new URL now)
+	return a.doRequestBootstrapServerOnboardingInfo(bootstrapURL)
 }
 
-func (a *Agent) doRequestBootstrapServerOnboardingInfo() error {
+func (a *Agent) doRequestBootstrapServerOnboardingInfo(bootstrapURL *string) error {
 	log.Println("[INFO] Starting the Request to get On-boarding Information.")
-	res, err := a.doTLSRequest(a.GetInputJSONContent(), a.GetBootstrapURL(), false)
+	res, err := a.doTLSRequest(a.GetInputJSONContent(), *bootstrapURL, false)
 	if err != nil {
 		log.Println("[ERROR] ", err.Error())
 		return err
 	}
 	log.Println("[INFO] Response retrieved successfully")
-	_ = a.doReportProgress(ProgressTypeBootstrapInitiated, "Bootstrap Initiated")
+	_ = a.doReportProgress(ProgressTypeBootstrapInitiated, "Bootstrap Initiated", bootstrapURL)
 	crypto := res.IetfSztpBootstrapServerOutput.ConveyedInformation
 	newVal, err := base64.StdEncoding.DecodeString(crypto)
 	if err != nil {
@@ -176,4 +197,188 @@ func (a *Agent) doRequestBootstrapServerOnboardingInfo() error {
 		return nil
 	}
 	return errri
+}
+
+//nolint:funlen
+func (a *Agent) downloadAndValidateImage(bootstrapURL *string) error {
+	log.Printf("[INFO] Starting the Download Image: %v", a.BootstrapServerOnboardingInfo.IetfSztpConveyedInfoOnboardingInformation.BootImage.DownloadURI)
+	_ = a.doReportProgress(ProgressTypeBootImageInitiated, "BootImage Initiated", bootstrapURL)
+	// Download the image from DownloadURI and save it to a file
+	a.BootstrapServerOnboardingInfo.IetfSztpConveyedInfoOnboardingInformation.InfoTimestampReference = fmt.Sprintf("%8d", time.Now().Unix())
+	for i, item := range a.BootstrapServerOnboardingInfo.IetfSztpConveyedInfoOnboardingInformation.BootImage.DownloadURI {
+		// TODO: maybe need to file download to a function in util.go
+		log.Printf("[INFO] Downloading Image %v", item)
+		// Create a empty file
+		file, err := os.Create(ARTIFACTS_PATH + a.BootstrapServerOnboardingInfo.IetfSztpConveyedInfoOnboardingInformation.InfoTimestampReference + filepath.Base(item))
+		if err != nil {
+			return err
+		}
+
+		caCert, _ := os.ReadFile(a.GetBootstrapTrustAnchorCert())
+		caCertPool := x509.NewCertPool()
+		caCertPool.AppendCertsFromPEM(caCert)
+		cert, _ := tls.LoadX509KeyPair(a.GetDeviceEndEntityCert(), a.GetDevicePrivateKey())
+
+		check := http.Client{
+			CheckRedirect: func(r *http.Request, _ []*http.Request) error {
+				r.URL.Opaque = r.URL.Path
+				return nil
+			},
+			Transport: &http.Transport{
+				TLSClientConfig: &tls.Config{
+					//nolint:gosec
+					InsecureSkipVerify: true, // TODO: remove skip verify
+					RootCAs:            caCertPool,
+					Certificates:       []tls.Certificate{cert},
+				},
+			},
+		}
+
+		response, err := check.Get(item)
+		if err != nil {
+			return err
+		}
+
+		sizeorigin, _ := strconv.Atoi(response.Header.Get("Content-Length"))
+		downloadSize := int64(sizeorigin)
+		log.Printf("[INFO] Downloading the image with size: %v", downloadSize)
+
+		if response.StatusCode != 200 {
+			return errors.New("received non 200 response code")
+		}
+		size, err := io.Copy(file, response.Body)
+		if err != nil {
+			return err
+		}
+		defer func() {
+			if err := file.Close(); err != nil {
+				log.Println("[ERROR] Error when closing:", err)
+			}
+		}()
+		defer func() {
+			if err := response.Body.Close(); err != nil {
+				log.Println("[ERROR] Error when closing:", err)
+			}
+		}()
+
+		log.Printf("[INFO] Downloaded file: %s with size: %d", ARTIFACTS_PATH+a.BootstrapServerOnboardingInfo.IetfSztpConveyedInfoOnboardingInformation.InfoTimestampReference+filepath.Base(item), size)
+		log.Println("[INFO] Verify the file checksum: ", ARTIFACTS_PATH+a.BootstrapServerOnboardingInfo.IetfSztpConveyedInfoOnboardingInformation.InfoTimestampReference+filepath.Base(item))
+		// TODO: maybe need to move sha calculatinos to a function in util.go
+		switch a.BootstrapServerOnboardingInfo.IetfSztpConveyedInfoOnboardingInformation.BootImage.ImageVerification[i].HashAlgorithm {
+		case "ietf-sztp-conveyed-info:sha-256":
+			f, err := os.Open(ARTIFACTS_PATH + a.BootstrapServerOnboardingInfo.IetfSztpConveyedInfoOnboardingInformation.InfoTimestampReference + filepath.Base(item))
+			if err != nil {
+				log.Panic(err)
+				return err
+			}
+			defer func() {
+				if err := f.Close(); err != nil {
+					log.Println("[ERROR] Error when closing:", err)
+				}
+			}()
+			h := sha256.New()
+			if _, err := io.Copy(h, f); err != nil {
+				return err
+			}
+			sum := fmt.Sprintf("%x", h.Sum(nil))
+			original := strings.ReplaceAll(a.BootstrapServerOnboardingInfo.IetfSztpConveyedInfoOnboardingInformation.BootImage.ImageVerification[i].HashValue, ":", "")
+			log.Println("calculated: " + sum)
+			log.Println("expected  : " + original)
+			if sum != original {
+				return errors.New("checksum mismatch")
+			}
+			log.Println("[INFO] Checksum verified successfully")
+			_ = a.doReportProgress(ProgressTypeBootImageComplete, "BootImage Complete", bootstrapURL)
+			return nil
+		default:
+			return errors.New("unsupported hash algorithm")
+		}
+	}
+	return nil
+}
+
+func (a *Agent) copyConfigurationFile(bootstrapURL *string) error {
+	log.Println("[INFO] Starting the Copy Configuration.")
+	_ = a.doReportProgress(ProgressTypeConfigInitiated, "Configuration Initiated", bootstrapURL)
+	// Copy the configuration file to the device
+	file, err := os.Create(ARTIFACTS_PATH + a.BootstrapServerOnboardingInfo.IetfSztpConveyedInfoOnboardingInformation.InfoTimestampReference + "-config")
+	if err != nil {
+		log.Println("[ERROR] creating the configuration file", err.Error())
+		return err
+	}
+	defer func() {
+		if err := file.Close(); err != nil {
+			log.Println("[ERROR] Error when closing:", err)
+		}
+	}()
+
+	plainTest, _ := base64.StdEncoding.DecodeString(a.BootstrapServerOnboardingInfo.IetfSztpConveyedInfoOnboardingInformation.Configuration)
+	_, err = file.WriteString(string(plainTest))
+	if err != nil {
+		log.Println("[ERROR] writing the configuration file", err.Error())
+		return err
+	}
+	// nolint:gosec
+	err = os.Chmod(ARTIFACTS_PATH+a.BootstrapServerOnboardingInfo.IetfSztpConveyedInfoOnboardingInformation.InfoTimestampReference+"-config", 0744)
+	if err != nil {
+		log.Println("[ERROR] changing the configuration file permission", err.Error())
+		return err
+	}
+	log.Println("[INFO] Configuration file copied successfully")
+	_ = a.doReportProgress(ProgressTypeConfigComplete, "Configuration Complete", bootstrapURL)
+	return nil
+}
+
+func (a *Agent) launchScriptsConfiguration(typeOf string, bootstrapURL *string) error {
+	var script, scriptName string
+	var reportStart, reportEnd ProgressType
+	switch typeOf {
+	case "post":
+		script = a.BootstrapServerOnboardingInfo.IetfSztpConveyedInfoOnboardingInformation.PostConfigurationScript
+		scriptName = "post"
+		reportStart = ProgressTypePostScriptInitiated
+		reportEnd = ProgressTypePostScriptComplete
+	default: // pre or default
+		script = a.BootstrapServerOnboardingInfo.IetfSztpConveyedInfoOnboardingInformation.PreConfigurationScript
+		scriptName = "pre"
+		reportStart = ProgressTypePreScriptInitiated
+		reportEnd = ProgressTypePreScriptComplete
+	}
+	log.Println("[INFO] Starting the " + scriptName + "-configuration.")
+	_ = a.doReportProgress(reportStart, "Report starting", bootstrapURL)
+	// nolint:gosec
+	file, err := os.Create(ARTIFACTS_PATH + a.BootstrapServerOnboardingInfo.IetfSztpConveyedInfoOnboardingInformation.InfoTimestampReference + scriptName + "configuration.sh")
+	if err != nil {
+		log.Println("[ERROR] creating the "+scriptName+"-configuration script", err.Error())
+		return err
+	}
+	defer func() {
+		if err := file.Close(); err != nil {
+			log.Println("[ERROR] Error when closing:", err)
+		}
+	}()
+
+	plainTest, _ := base64.StdEncoding.DecodeString(script)
+	_, err = file.WriteString(string(plainTest))
+	if err != nil {
+		log.Println("[ERROR] writing the "+scriptName+"-configuration script", err.Error())
+		return err
+	}
+	// nolint:gosec
+	err = os.Chmod(ARTIFACTS_PATH+a.BootstrapServerOnboardingInfo.IetfSztpConveyedInfoOnboardingInformation.InfoTimestampReference+scriptName+"configuration.sh", 0755)
+	if err != nil {
+		log.Println("[ERROR] changing the "+scriptName+"-configuration script permission", err.Error())
+		return err
+	}
+	log.Println("[INFO] " + scriptName + "-configuration script created successfully")
+	cmd := exec.Command("/bin/sh", ARTIFACTS_PATH+a.BootstrapServerOnboardingInfo.IetfSztpConveyedInfoOnboardingInformation.InfoTimestampReference+scriptName+"configuration.sh") //nolint:gosec
+	out, err := cmd.Output()
+	if err != nil {
+		log.Println("[ERROR] running the "+scriptName+"-configuration script", err.Error())
+		return err
+	}
+	log.Println(string(out)) // remove it
+	_ = a.doReportProgress(reportEnd, "Report end", bootstrapURL)
+	log.Println("[INFO] " + scriptName + "-Configuration script executed successfully")
+	return nil
 }
